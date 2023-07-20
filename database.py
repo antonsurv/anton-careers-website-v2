fa5036a4da9aa8ebb5179e6d9dd688b0bc18ccef
antonsurv <51750998+antonsurv@users.noreply.github.com>
2023-07-20 04:26:57 +0000

    added result.mappings()

--- database.py
+++ database.py
@@ -16,6 +16,24 @@ def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = []
-    for row in result.all():
+    for row in result.mappings():
       jobs.append(dict(row))
     return jobs
+
+# with engine.connect() as conn:
+#   result = conn.execute(text("select * from jobs"))
+
+#   result_dicts = []
+#   for row in result.all():
+#     result_dicts.append(dict(row._mapping))
+  
+#   print(result_dicts)
+
+# with engine.connect() as conn:
+#   result = conn.execute(text("select * from jobs"))
+
+#   result_dicts = []
+#   for row in result.mappings():
+#     result_dicts.append(dict(row))
+  
+#   print(result_dicts)
